ab4863bab4ab346f089931c57a9a04a070e97eb1
Jasmine Fan <jf4645@nyu.edu>
2026-08-27 09:03:00 -0400

    finish register

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -25,4 +25,6 @@ urlpatterns = [
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('', TemplateView.as_view(template_name="Home.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
+    path('login/', TemplateView.as_view(template_name="index.html")),
+    path('register/', TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
